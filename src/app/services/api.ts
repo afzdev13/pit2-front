3017bf7acbd73a6042cd3ebe9cd9c7f9355f73f4
João Afonso <joaosilvaafonso14@gmail.com>
2023-12-05 21:10:54 -0400

    first commit

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'http://18.224.94.36:3000'
 });
 
 api.interceptors.request.use(async (config: any) => {
